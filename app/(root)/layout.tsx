29db6cbdebd8bbc33ba0722f51bd4df6a3463d1c
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-24 18:57:17 -0400

    Create layout.tsx file inside of (root) folder

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+    </html>
+  );
+}
