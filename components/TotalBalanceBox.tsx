8f0eca3ada69ea3f8bfc7792ae31484cf288e41f
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-25 12:59:45 -0400

    feat: Add total current balance to TotalBalanceBox component

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -13,6 +13,17 @@ const TotalBalanceBox = ({
         <h2 className='header-2'>
           Banks Account: {totalBanks}
         </h2>
+
+        <div className='flex flex-col gap-2'>
+          <p className='total-balance-label'>
+            Total Current Balance
+          </p>
+
+          <p className='total-balance-amount flex-center gap-2'>
+            {totalCurrentBalance}
+          </p>
+        </div>
+
       </div>
     </section>
   )
