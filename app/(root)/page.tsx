5d1a574be1dfa6fd5d416b0d5474273dae8b4b55
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-24 22:05:25 -0400

    feat: Add HeaderBox component and update home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
 
 const Home = () => {
   return (
     <section className='home'>
       <div className='home-content'>
         <header className='home-header'>
-          Welcome, Javier
+          <HeaderBox>
         </header>
       </div>
     </section>
