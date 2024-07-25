55b3b21dcbcf4b7f3b3e6b9f7fa26f987e03c9fb
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-25 12:05:07 -0400

    feat: Add TotalBalanceBox component to home page

---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TotalBalanceBox = ({
+  accounts = [], totalBanks, totalCurrentBalance
+}: TotlaBalanceBoxProps) => {
+  return (
+    <section className='total-balance'>
+
+    </section>
+  )
+}
+
+export default TotalBalanceBox
