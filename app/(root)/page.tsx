2f9162ed57c6d7f7d4fc534873bd9a057459c512
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-24 22:12:02 -0400

    refactor: Update Home page header with dynamic greeting

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,19 @@
 import HeaderBox from '@/components/HeaderBox'
 
 const Home = () => {
+
+  const loggedIn = {firstName: 'Javier'};
+
   return (
     <section className='home'>
       <div className='home-content'>
         <header className='home-header'>
-          <HeaderBox>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your accounts and transactions efficiently"
+          />
         </header>
       </div>
     </section>
