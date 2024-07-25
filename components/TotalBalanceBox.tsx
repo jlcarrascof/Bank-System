3d3722cd9622b29792fa1953fdbf4e8319d256b3
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-25 12:27:11 -0400

    feat: Add doughnut chart and bank account balance to TotalBalanceBox component

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -5,7 +5,15 @@ const TotalBalanceBox = ({
 }: TotlaBalanceBoxProps) => {
   return (
     <section className='total-balance'>
+      <div className='total-balance-chart'>
+        {/* Doughnut chart */}
+      </div>
 
+      <div className='flex flex-col gap-6'>
+        <h2 className='header-2'>
+          Banks Account: {totalBanks}
+        </h2>
+      </div>
     </section>
   )
 }
