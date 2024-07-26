c5f5bb30ff38f149251b76ee7d954c10a71ae962
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-26 18:34:33 -0400

    refactor: Update DoughnutChart component to include data prop

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -6,7 +6,19 @@ import { Doughnut } from 'react-chartjs-2';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const DoughnutChart = ({accounts}: DoughnutChartProps) => {
-  return <Doughnut data={[]} />
+
+  const data = {
+    datasets: [
+      {
+        label: 'Banks',
+        data: [1250, 2500, 3750],
+        backgroundColor: ['#0747b6', '#2265d8', '#2f91fa']
+      }
+
+    ]
+  }
+
+  return <Doughnut data={data} />
 }
 
 export default DoughnutChart;
