684aca41ae52694dcdff8b6651dda3d45f10a9fb
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-26 14:51:03 -0400

    feat: Add Chart.js and DoughnutChart component

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -1,3 +1,8 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend} from 'chart.js'
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 const DoughnutChart = () => {
   return (
     <div>DoughnutChart</div>
