12be43ff7c09d7f25a2246f6dfeb3a3f818878dc
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-26 22:53:53 -0400

    refactor: Update Sidebar component to include Horizon logo

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link"
+import Image from "next/image"
+
 const Sidebar = ({user}: SiderbarProps) => {
   return (
     <section className='sidebar'>
         <nav className='flex flex-col gap-4'>
-
+            <Link href="/" className='mb-12 cursor-pointer items-center gap-2'>
+                <Image src='/icons/logo.svg' width={34} height={34} alt='Horizon logo' className="size-[24px] max-xl:size-14"  />
+            </Link>
         </nav>
     </section>
   )
